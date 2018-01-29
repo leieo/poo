b6849d46f9740f84d64cfd80728cfcbebb2921bb
leieo <leila.el.omari@gmail.com>
2018-01-29 15:30:07 +0100

    getter

--- bankAccount.php
+++ bankAccount.php
@@ -9,16 +9,25 @@ class BankAccount
 		$this->balance = $firstDeposit;
 	}
 
-// on crée un setter, c'est à dire une fonction
-// par convention, on le nomme avec le prefixe "set", mais on peut le nommer autrement.
+// on crée un setter (c'est à dire une fonction)
+// par convention, on met le prefixe "set", mais on peut le nommer autrement
+// ici, on s'assure d'un montant minimum avant d'autoriser à modifier le solde
 	public function setBalance($newBalance) {
-		if($newBalance < 10000) {
+		if($newBalance < 100) {
 			throw new Exception("Montant trop faible");
 		}
 		$this->balance = $newBalance;
 	}
+
+// on crée un getter (c'est à dire une fonction)
+// par convention, on met le prefixe "set", mais on peut le nommer autrement
+// ici, il permet de multiplier le solde par 100 pour l'exprimer en cents
+	public function getBalance() {
+		return $this->balance * 100;
+	}
 }
 
 $compteFaiza = new BankAccount("068794563645", 100);
-$compteFaiza->setBalance(50000);
-var_dump($compteFaiza->balance);
+$compteFaiza->setBalance(500);
+echo $compteFaiza->balance . ' dollars' . PHP_EOL;
+echo $compteFaiza->getBalance() . ' cents' . PHP_EOL;
--- person.php
+++ person.php
@@ -14,13 +14,17 @@ class Person {
 	public function fullName() {
 		return sprintf("%s %s", $this->firstName, $this->lastName);
 	}
+
+	public function getAge() {
+		return $this->age * 365;
+	}
 }
 
 $faiza = new Person('Faïza', 'Guène', 32);
-$audre = new Person('Audre', 'Lorde', 68);
+$audre = new Person('Audre', 'Lorde', 84);
 
 $fullNameFaiza = $faiza->fullName();
 $fullNameAudre = $audre->fullName();
 
-echo $fullNameAudre . PHP_EOL;
-echo $fullNameFaiza . PHP_EOL;
+echo $fullNameAudre . ' aurait ' . $audre->age . ' ans.' . PHP_EOL;
+echo $fullNameFaiza . ' a vécu ' . $faiza->getAge() . ' jours.' . PHP_EOL;
