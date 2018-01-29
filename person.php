bcca470c3ed14e0c0e899cdbd319b86e353ad575
leieo <leila.el.omari@gmail.com>
2018-01-29 17:01:00 +0100

    variables statiques

--- person.php
+++ person.php
@@ -4,11 +4,17 @@ class Person {
 	public $firstName;
 	public $lastName;
 	public $age;
+	private static $totalCount = 0;
 
 	public function __construct($firstName, $lastName, $yearsold) {
 		$this->firstName = $firstName;
 		$this->lastName = $lastName;
 		$this->age = $yearsold;
+		static::$totalCount++;
+	}
+
+	public static function getTotalCount() {
+		return static::$totalCount;
 	}
 
 	public function fullName() {
@@ -18,13 +24,23 @@ class Person {
 	public function getAge() {
 		return $this->age * 365;
 	}
+
+	public function setAge($newAge) {
+		if($age > 0) {
+			$this->age = $newAge;
+		}
+	}
 }
 
 $faiza = new Person('Faïza', 'Guène', 32);
 $audre = new Person('Audre', 'Lorde', 84);
+$lei = new Person('Leï', 'Omari', 29);
 
 $fullNameFaiza = $faiza->fullName();
 $fullNameAudre = $audre->fullName();
 
 echo $fullNameAudre . ' aurait ' . $audre->age . ' ans.' . PHP_EOL;
 echo $fullNameFaiza . ' a vécu ' . $faiza->getAge() . ' jours.' . PHP_EOL;
+
+echo Person::getTotalCount() . PHP_EOL;
+echo Person::$totalCount . PHP_EOL;
